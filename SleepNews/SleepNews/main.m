56f659412c060f766d6e6bf81e4c1e764be1f86a
efairy520 <efairy520@qq.com>
2016-12-04 10:45:27 +0800

    创建项目

---
+++ SleepNews/SleepNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SleepNews
+//
+//  Created by Kavee DJ on 2016/12/4.
+//  Copyright © 2016年 Kavee DJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

e8b58e70b6208d183eec3e481d24c4e8ad37efb6
efairy520 <efairy520@qq.com>
2016-12-04 12:56:45 +0800

    划分项目结构

---
+++ SleepNews/SleepNews/Class/Main/TabbarViewController.h
@@ -0,0 +1,14 @@
+//
+//  TabbarViewController.h
+//  SleepNews
+//
+//  Created by Kavee DJ on 2016/12/4.
+//  Copyright © 2016年 Kavee DJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabbarViewController : UITabBarController
+- (void)selectIndex:(int)index;
+
+@end
